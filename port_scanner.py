48f3d7fd8d3e348bcf54e62c3e4d59413564ca21
Plasmatic <moses@mosesxu.net>
2020-01-19 00:48:32 -0500

    initial files

---
+++ port_scanner.py
@@ -0,0 +1,65 @@
+import json
+import socket
+import subprocess
+import sys
+from datetime import datetime
+
+# init stuff
+start = datetime.now()
+with open('config.json') as f:
+    config = json.loads(f.read())
+
+"""
+Config options:
+
+Host: Host ip or domain
+
+Common Ports: List of common ports to check first
+Other (Priority) Ports: List of other ports to check first (after checking common ports).  These ports should not be commonly used ones
+
+Show Closed Ports: (true or false) whether to output closed ports (as closed) or whether to just not output them at all
+"""
+
+common_ports = config['common_ports']
+other_priority_ports = config['other_ports']
+other_ports = list(set(list(range(1, 65536))) - set(common_ports) - set(other_priority_ports))
+all_ports = [
+    ['Common Ports', common_ports],
+    ['Other Priority Ports', other_priority_ports],
+    ['All Other Ports', other_ports]
+]
+
+host = config['host']
+show_closed_ports = config['show_closed_ports']
+
+# Print initializing info
+print('Initialized port scanner...')
+print(f'Host: "{host}"')
+print(f'Common Ports: {common_ports}')
+print(f'Other Priority Ports: {other_priority_ports}')
+print()
+
+# Scan ports
+for category, ports in all_ports:
+    print(f'-=[ Scanning {category} ]=-')
+    for port in ports:  
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = sock.connect_ex((host, port))
+            if result == 0:
+                print(f'Port {port} open')
+            elif show_closed_ports:
+                print(f'Port {port} closed')
+            sock.close()
+        except socket.gaierror:
+            print('Could not resolve hostname. Terminating program...')
+            sys.exit()
+        except socket.error as e:
+            print(f'Connection failed.  Error: {e}')
+            sys.exit()
+    
+    print() # Formatting
+
+# Print total run time
+total_elapased = datetime.now() - start
+print(f'Scanning completed in {total_elapsed}')
